6ece2eeb50cdd13def4727fea95fb5f6447cd41b
Litzi Sanchez <77827671+LitziSanchez@users.noreply.github.com>
2024-06-27 20:20:40 -0500

    Portafolio actualizado

---
+++ app.js
@@ -0,0 +1,28 @@
+//Selecciones para barra de navegacion//
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+//cerrar boton
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+})
+
+//Seleccion de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+//Funcionalidad de desplazar hacia arriba
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth",
+    });
+});
